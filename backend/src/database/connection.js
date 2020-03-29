c198221f5602d08082c001ef2fc731a83f8012da
blciampi <blciampi@hotmail.com>
2020-03-29 16:47:20 -0300

    Funcionalidades avançadas

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,10 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.development);
+//define se a configuração do banco será de teste ou desenvolvimento
+//variável de ambiente NODE_ENV definida no package.json
+const config = process.env.NODE_ENV == 'test' ? configuration.test : configuration.development;
+
+const connection = knex(config);
 
 module.exports = connection;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const ongController = require('./controllers/OngController')
 const incidentController = require('./controllers/IncidentController')
@@ -8,14 +9,57 @@ const sessionController = require('./controllers/SessionController')
 const routes = express.Router();
 
 routes.get('/ongs', ongController.index);
-routes.post('/ongs', ongController.create);
 
-routes.post('/incidents', incidentController.create);
-routes.get('/incidents', incidentController.index);
-routes.delete('/incidents/:id', incidentController.delete);
+//Validação de cadastro da ong
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), ongController.create);
 
-routes.get('/profile', profileController.index);
+//Validação na criação de um caso
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown(),
 
-routes.post('/session', sessionController.create);
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required()
+  })
+}), incidentController.create);
+
+//Validação da paginação
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}), incidentController.index);
+
+//Validação ao apagar um caso
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), incidentController.delete);
+
+//Validação ao logar
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown()
+}), profileController.index);
+
+//Validação ao realizar login
+routes.post('/session', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown()
+}), sessionController.create);
 
 module.exports = routes;
